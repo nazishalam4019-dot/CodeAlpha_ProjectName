759a32814aa3d44dfaa8434111f4e44ff3d2d20f
naziish01 <nazishalam4019@gmail.com>
2026-01-11 12:55:10 +0530

    CodeAlpha_ProjectName
    1st and 2nd tasks to CodeAlpha_ProjectName

---
+++ python.py
@@ -0,0 +1,33 @@
+print("Hello world","Jamia hamdard")
+print(23+54)
+
+name = "Nazish"
+age = 20
+price = 25.99
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+age=23
+old=False
+a=None
+print(type(old))
+print(type(a))
+
+a=4
+b=7
+print(a+b)
+
+a=2
+b=3
+txt="@"
+print(2*txt*3)
+
+a,b=2,3
+c=4
+print(a+b*c)
+
+a,b=1.5,3
+c=a//b
+print(c,a/b)
